e81fad776733b18bdfa248d9b64f640caacf19b2
felipeesteban6 <femaldonadoc@gmail.com>
2026-08-10 22:58:12 -0400

    Enlarge login logo, keep credentials card compact

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -6,12 +6,12 @@ import { Link } from '@inertiajs/vue3';
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div>
             <Link href="/">
-                <img src="/Img/fm-logo-completo.svg" alt="Felipe Maldonado" class="w-48 h-auto" />
+                <img src="/Img/fm-logo-completo.svg" alt="Felipe Maldonado" class="w-[550px] h-auto" />
             </Link>
         </div>
 
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+            class="w-full sm:max-w-sm mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
         >
             <slot />
         </div>
